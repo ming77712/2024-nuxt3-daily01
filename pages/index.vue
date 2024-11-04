a4781e517c2c7eb354c2e19278e7f13f82d43336
Ming <a955269@gmail.com>
2024-11-04 23:53:19 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    Page: 首頁
+    <p>目前路由的路徑 - route.fullPath 為: {{ route.fullPath }}</p>
+  </div>
+</template>
+
+<style scoped></style>
