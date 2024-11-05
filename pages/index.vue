ad0d679365d232059f4768a4dc03e624644e7cc3
Ming <a955269@gmail.com>
2024-11-06 00:44:36 +0800

    Day 2

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,53 @@ const route = useRoute();
 </script>
 
 <template>
-  <div>
+  <h1>
     Page: 首頁
-    <p>目前路由的路徑 - route.fullPath 為: {{ route.fullPath }}</p>
-  </div>
+  </h1>
+  <p>目前路由的路徑 - route.fullPath 為: {{ route.fullPath }}</p>
+  <button
+    type="button"
+    class="btn btn-primary"
+  >Primary</button>
+  <button
+    type="button"
+    class="btn btn-secondary"
+  >Secondary</button>
+  <button
+    type="button"
+    class="btn btn-success"
+  >Success</button>
+  <button
+    type="button"
+    class="btn btn-danger"
+  >Danger</button>
+  <button
+    type="button"
+    class="btn btn-warning"
+  >Warning</button>
+  <button
+    type="button"
+    class="btn btn-info"
+  >Info</button>
+  <button
+    type="button"
+    class="btn btn-light"
+  >Light</button>
+  <button
+    type="button"
+    class="btn btn-dark"
+  >Dark</button>
+
+  <button
+    type="button"
+    class="btn btn-link"
+  >Link</button>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+$primary: blue;
+
+h1 {
+  color: $primary;
+}
+</style>
