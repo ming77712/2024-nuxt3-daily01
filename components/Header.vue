a1855b92def68d25df48ef041a78d072367b227f
Ming <a955269@gmail.com>
2024-11-07 23:23:28 +0800

    Day 4

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ul class="list">
+    <li>
+      <NuxtLink to="/">前台首頁</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/about">前台 About</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/admin">前往 後台首頁</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/admin/order">前往 後台訂單</NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,58 @@
 <script lang="ts" setup></script>
 
 <template>
-  <header>
-    <ul class="list">
-      <li>
-        <NuxtLink to="/">前台首頁</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">前台 About</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/admin">前往 後台首頁</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/admin/order">前往 後台訂單</NuxtLink>
-      </li>
-    </ul>
-  </header>
+  <Header />
   <h1>
     前台首頁
   </h1>
 
-  <button
-    type="button"
-    class="btn btn-primary"
-  >Primary</button>
-  <button
-    type="button"
-    class="btn btn-secondary"
-  >Secondary</button>
-  <button
-    type="button"
-    class="btn btn-success"
-  >Success</button>
-  <button
-    type="button"
-    class="btn btn-danger"
-  >Danger</button>
-  <button
-    type="button"
-    class="btn btn-warning"
-  >Warning</button>
-  <button
-    type="button"
-    class="btn btn-info"
-  >Info</button>
-  <button
-    type="button"
-    class="btn btn-light"
-  >Light</button>
-  <button
-    type="button"
-    class="btn btn-dark"
-  >Dark</button>
-  <button
-    type="button"
-    class="btn btn-link"
-  >Link</button>
+  <ProductCard
+    imageSrc="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/typescript-hotel/%E6%A1%8C%E6%A9%9F%E7%89%88/room2-1.png"
+    altText="享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢"
+    title="尊爵雙人房"
+    price="NT$10000"
+    buttonText="馬上預約"
+  />
+
+  <div class="d-flex gap-3">
+    <button
+      type="button"
+      class="btn btn-primary"
+    >Primary</button>
+    <button
+      type="button"
+      class="btn btn-secondary"
+    >Secondary</button>
+    <button
+      type="button"
+      class="btn btn-success"
+    >Success</button>
+    <button
+      type="button"
+      class="btn btn-danger"
+    >Danger</button>
+    <button
+      type="button"
+      class="btn btn-warning"
+    >Warning</button>
+    <button
+      type="button"
+      class="btn btn-info"
+    >Info</button>
+    <button
+      type="button"
+      class="btn btn-light"
+    >Light</button>
+    <button
+      type="button"
+      class="btn btn-dark"
+    >Dark</button>
+    <button
+      type="button"
+      class="btn btn-link"
+    >Link</button>
+  </div>
+
   <footer>
     <h5>
       前台模板的表尾
